85678bbf0b1cd516c86203439fe59363f1bc3ea6
Archana <archana.patel@eronkan.com>
2022-08-26 10:59:51 +0530

    constructor using function in it

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ function Book(authorName, title, numberOfPages, ifRead) {
     this.title = title
     this.ifRead =ifRead
     this.numberOfPages=numberOfPages;
+
+    this.info=function(){
+        return `${ifRead}...this has been read`
+    }
 }
 
   
   const book1 = new Book('steve', 'paradox','19',"yes")
   const book2 = new Book('jhon steve', 'constant','836','no');
+  console.log(book2.info())
  console.log(book1.authorName);
  console.log(book2.ifRead);
  console.log(book1.title);
